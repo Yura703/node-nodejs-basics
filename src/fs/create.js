34c62b31ac919fa399c5abb9cb7ea1e7851154fc
Yura Lapitski <lyurik@tut.by>
2022-06-01 21:11:32 +0300

    init: start task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,30 @@
+/*  
+    реализовать функцию, которая создает новый файл 'fresh.txt' с 
+    содержимым 'I am fresh and young' внутри 'files' папки (если файл уже существует, 
+    Error сообщение 'FS operation failed' должно быть выброшено)
+*/
+
+import fs from 'fs';
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { log } from 'console';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const create = async () => {
-    // Write your code here 
+    try {
+        const pathFile = join( __dirname, 'files', 'fresh.txt');
+
+        const fileHandle = await fs.promises.open(pathFile, 'wx').catch (() => {
+            throw new Error('FS operation failed');                   
+            });
+        fileHandle.write('I am fresh and young');
+        console.log('\x1b[32m' + 'File created successfully' + '\x1b[0m');
+    } catch (error) {        
+        console.log('\x1b[31m' + error + '\x1b[0m');
+    }   
 };
+
+create();
