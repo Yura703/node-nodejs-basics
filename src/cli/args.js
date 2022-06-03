29fb756066603f2683313b6c163fbaa9a8afd207
Yura Lapitski <lyurik@tut.by>
2022-06-03 16:04:55 +0300

    feat: finished tasks CLI

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,9 +1,20 @@
 /*
 args.js- реализовать функцию, которая анализирует аргументы командной строки 
 (данные в формате --propName value --prop2Name value2, вам не нужно его проверять) 
-и выводит их на консоль в форматеpropName is value, prop2Name is value2
+и выводит их на консоль в формате propName is value, prop2Name is value2
 */
+import { argv } from 'process';
 
 export const parseArgs = () => {
-    // Write your code here 
+    try {
+        const valueOne = argv[argv.indexOf('--propName') + 1];
+        const valueTwo = argv[argv.indexOf('--prop2Name') + 1];
+
+        console.log(`PropName is ${valueOne}, prop2Name is ${valueTwo}`); 
+               
+    } catch (error) {
+        console.log('Error' + error);
+    }    
 };
+
+parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,8 +1,23 @@
 /*
-env.js- реализовать функцию, которая анализирует переменные окружения 
+env.js- реализовать функцию, которая меняет переменные окружения на
 с префиксом RSS_и выводит их на консоль в форматеRSS_name1=value1; RSS_name2=value2
 */
 
 export const parseEnv = () => {
-    // Write your code here 
+  try {
+    let str = '';
+
+    for (const obj in process.env) {      
+      const objName = `RSS_${obj}`;
+      const objKey = process.env[obj];
+      str += `${objName}=${objKey};\n`;
+    }
+
+    console.log(str.slice(0, -2));
+    
+  } catch (error) {
+    console.log('Error: ' + error);
+  }
 };
+
+parseEnv();
