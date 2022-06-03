bf578ea831e6b7333ebb940a5595322e300783cc
Yura Lapitski <lyurik@tut.by>
2022-06-03 15:22:35 +0300

    feat: finished task HASH

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,9 @@
+/*
+args.js- реализовать функцию, которая анализирует аргументы командной строки 
+(данные в формате --propName value --prop2Name value2, вам не нужно его проверять) 
+и выводит их на консоль в форматеpropName is value, prop2Name is value2
+*/
+
 export const parseArgs = () => {
     // Write your code here 
 };
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,8 @@
+/*
+env.js- реализовать функцию, которая анализирует переменные окружения 
+с префиксом RSS_и выводит их на консоль в форматеRSS_name1=value1; RSS_name2=value2
+*/
+
 export const parseEnv = () => {
     // Write your code here 
 };
